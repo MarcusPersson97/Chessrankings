1191df9621c2926fb8378786e4f36cba7fa4af59
MarcusPersson97 <marcus.persson.97@hotmail.com>
2026-04-21 17:22:03 +0200

    Created the folder structure and added a server component

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+})
+
+
+app.listen(port, () => {
+  console.log('Server is now running');
+});
