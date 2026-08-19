4d936dc2fbf6e9aa1894f30339e4da12cd95e03f
MarcusPersson97 <marcus.persson.97@hotmail.com>
2026-08-19 17:48:42 +0200

    updated playerlist to now include loading messages, error messages and polling that updates the ranking list every 30secs

--- View/frontend/src/components/playerlist.jsx
+++ View/frontend/src/components/playerlist.jsx
@@ -3,31 +3,57 @@ import PlayerRow from "./playerrow";
 
 function PlayerList() {
     const [players, setPlayers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    useEffect(() => {
         async function fetchPlayers() {
+        try {
+            setLoading(true);
+            setError(null);
+
             const response = await fetch("http://localhost:3000/api/players");
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch players");
+            }
+
             const data = await response.json();
 
             setPlayers(data);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
         }
+    }
 
-        fetchPlayers();
+    useEffect(() => {
+    
+    fetchPlayers();
+    const interval = setInterval(fetchPlayers, 30000);
+    return() => clearInterval(interval);
+
     }, []);
 
     return (
-        <div>
-            <h2>Chess Rankings</h2>
+    <div>
+        <h2>Chess Rankings</h2>
 
-            {players.map((player, index) => (
+        {loading && <p>Loading players...</p>}
+
+        {error && <p>{error}</p>}
+
+        {!loading && !error && (
+            players.map((player, index) => (
                 <PlayerRow
                     key={player._id}
                     player={player}
                     rank={index + 1}
                 />
-            ))}
-        </div>
-    );
+            ))
+        )}
+    </div>
+);
 }
 
 export default PlayerList;
