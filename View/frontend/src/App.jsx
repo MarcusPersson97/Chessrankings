c08d66b45869cc38bc402eca19d53688d2721c59
MarcusPersson97 <marcus.persson.97@hotmail.com>
2026-08-20 00:45:14 +0200

    refactored some of the code from playerlist to app to make it more scalable with other features

--- View/frontend/src/App.jsx
+++ View/frontend/src/App.jsx
@@ -1,12 +1,51 @@
+import { useEffect, useState } from "react";
 import PlayerList from "./components/playerlist";
 
 function App() {
+
+    const [players, setPlayers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    async function fetchPlayers() {
+        try {
+            setLoading(true);
+            setError(null);
+
+            const response = await fetch("http://localhost:3000/api/players");
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch players");
+            }
+
+            const data = await response.json();
+
+            setPlayers(data);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchPlayers();
+
+        const interval = setInterval(fetchPlayers, 30000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div>
             <h1>Fullstack Lab</h1>
-          <PlayerList />
+            <PlayerList
+                players={players}
+                loading={loading}
+                error={error}
+            />
         </div>
-    )
+    );
 }
 
 export default App;
--- View/frontend/src/components/playerlist.jsx
+++ View/frontend/src/components/playerlist.jsx
@@ -1,40 +1,8 @@
 import { useEffect, useState } from "react";
 import PlayerRow from "./playerrow";
 
-function PlayerList() {
-    const [players, setPlayers] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-        async function fetchPlayers() {
-        try {
-            setLoading(true);
-            setError(null);
-
-            const response = await fetch("http://localhost:3000/api/players");
-
-            if (!response.ok) {
-                throw new Error("Failed to fetch players");
-            }
-
-            const data = await response.json();
-
-            setPlayers(data);
-        } catch (error) {
-            setError(error.message);
-        } finally {
-            setLoading(false);
-        }
-    }
-
-    useEffect(() => {
+function PlayerList({players, loading, error}) {
     
-    fetchPlayers();
-    const interval = setInterval(fetchPlayers, 30000);
-    return() => clearInterval(interval);
-
-    }, []);
-
     return (
     <div>
         <h2>Chess Rankings</h2>
