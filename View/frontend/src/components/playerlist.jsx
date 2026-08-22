5abc030a9f4b893ecaa5b564dc279c1cf6422425
MarcusPersson97 <marcus.persson.97@hotmail.com>
2026-08-22 17:47:27 +0200

    added a search box for players where players get filtered based on whats in the box

--- View/frontend/src/components/playerlist.jsx
+++ View/frontend/src/components/playerlist.jsx
@@ -3,17 +3,29 @@ import PlayerRow from "./playerrow";
 
 function PlayerList({players, loading, error, fetchPlayers}) {
 
+    const [search, setSearch] = useState("");
     
+    const filteredPlayers = players.filter(player =>
+    player.name.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
     <div>
         <h2>Chess Rankings</h2>
+
+        <input
+        type="text"
+        placeholder="Search players..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        />
 
         {loading && <p>Loading players...</p>}
 
         {error && <p>{error}</p>}
 
         {!loading && !error && (
-            players.map((player, index) => (
+            filteredPlayers.map((player, index) => (
                 <PlayerRow
                     key={player._id}
                     player={player}
